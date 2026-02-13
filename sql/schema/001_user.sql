31c87c52ba9386bd010853496dc6e3bfcb919c29
vaxxnsh <kakshit817@gmail.com>
2026-02-13 23:34:23 +0530

    feat: updated user schema for auth

--- sql/schema/001_user.sql
+++ sql/schema/001_user.sql
@@ -3,10 +3,13 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL,
-    password Text NOT NULL,
+    password TEXT NOT NULL,
+    avatar_id UUID,
+    role TEXT NOT NULL DEFAULT 'user',
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
+
 
 -- +goose Down
 
