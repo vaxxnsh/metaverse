ff6670222480c5c5fb0dfd8e810ebac34e3b8635
vaxxnsh <kakshit817@gmail.com>
2026-02-17 00:24:50 +0530

    test: added tests for arena related endpoints

--- tests/user_test.go
+++ tests/user_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func doRequest(t *testing.T, method, url string, body interface{}, token string) (*http.Response, map[string]interface{}) {
+func doRequest(t *testing.T, method, url string, body any, token string) (*http.Response, map[string]any) {
 	var reqBody io.Reader
 
 	if body != nil {
@@ -37,7 +37,7 @@ func doRequest(t *testing.T, method, url string, body interface{}, token string)
 
 	respBody, _ := io.ReadAll(resp.Body)
 
-	var result map[string]interface{}
+	var result map[string]any
 	json.Unmarshal(respBody, &result)
 
 	return resp, result
@@ -47,7 +47,7 @@ func TestSignupOnlyOnce(t *testing.T) {
 	username := randomUsername()
 	password := "123456"
 
-	resp, _ := doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]interface{}{
+	resp, _ := doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]any{
 		"username": username,
 		"password": password,
 		"type":     "admin",
@@ -57,7 +57,7 @@ func TestSignupOnlyOnce(t *testing.T) {
 		t.Fatalf("expected 200 got %d", resp.StatusCode)
 	}
 
-	resp2, _ := doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]interface{}{
+	resp2, _ := doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]any{
 		"username": username,
 		"password": password,
 		"type":     "admin",
@@ -72,13 +72,13 @@ func TestSigninSuccess(t *testing.T) {
 	username := randomUsername()
 	password := "123456"
 
-	doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]interface{}{
+	doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]any{
 		"username": username,
 		"password": password,
 		"type":     "admin",
 	}, "")
 
-	resp, data := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]interface{}{
+	resp, data := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]any{
 		"username": username,
 		"password": password,
 	}, "")
@@ -96,13 +96,13 @@ func TestSigninFailure(t *testing.T) {
 	username := randomUsername()
 	password := "123456"
 
-	doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]interface{}{
+	doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]any{
 		"username": username,
 		"password": password,
 		"type":     "admin",
 	}, "")
 
-	resp, _ := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]interface{}{
+	resp, _ := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]any{
 		"username": "wrongUser",
 		"password": password,
 	}, "")
@@ -116,27 +116,27 @@ func TestUserMetadataUpdate(t *testing.T) {
 	username := randomUsername()
 	password := "123456"
 
-	doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]interface{}{
+	doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]any{
 		"username": username,
 		"password": password,
 		"type":     "admin",
 	}, "")
 
-	_, signinData := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]interface{}{
+	_, signinData := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]any{
 		"username": username,
 		"password": password,
 	}, "")
 
 	token := signinData["token"].(string)
 
-	_, avatarData := doRequest(t, "POST", BACKEND_URL+"/api/v1/admin/avatar", map[string]interface{}{
+	_, avatarData := doRequest(t, "POST", BACKEND_URL+"/api/v1/admin/avatar", map[string]any{
 		"imageUrl": "https://test.com/image.png",
 		"name":     "Timmy",
 	}, token)
 
 	avatarId := avatarData["avatarId"].(string)
 
-	resp, _ := doRequest(t, "POST", BACKEND_URL+"/api/v1/user/metadata", map[string]interface{}{
+	resp, _ := doRequest(t, "POST", BACKEND_URL+"/api/v1/user/metadata", map[string]any{
 		"avatarId": avatarId,
 	}, token)
 
@@ -149,20 +149,20 @@ func TestCreateAndDeleteSpace(t *testing.T) {
 	username := randomUsername()
 	password := "123456"
 
-	doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]interface{}{
+	doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]any{
 		"username": username,
 		"password": password,
 		"type":     "user",
 	}, "")
 
-	_, signinData := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]interface{}{
+	_, signinData := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]any{
 		"username": username,
 		"password": password,
 	}, "")
 
 	token := signinData["token"].(string)
 
-	_, spaceData := doRequest(t, "POST", BACKEND_URL+"/api/v1/space", map[string]interface{}{
+	_, spaceData := doRequest(t, "POST", BACKEND_URL+"/api/v1/space", map[string]any{
 		"name":       "Test",
 		"dimensions": "100x200",
 	}, token)
@@ -174,4 +174,150 @@ func TestCreateAndDeleteSpace(t *testing.T) {
 	if resp.StatusCode != 200 {
 		t.Fatalf("expected 200 got %d", resp.StatusCode)
 	}
+}
+
+func TestArenaEndpoints(t *testing.T) {
+
+	username := randomUsername()
+	password := "123456"
+
+	_, signupData := doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]interface{}{
+		"username": username,
+		"password": password,
+		"type":     "admin",
+	}, "")
+
+	adminId := signupData["userId"]
+
+	_, signinData := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]interface{}{
+		"username": username,
+		"password": password,
+	}, "")
+
+	adminToken := signinData["token"].(string)
+
+	_, userSignupData := doRequest(t, "POST", BACKEND_URL+"/api/v1/signup", map[string]interface{}{
+		"username": username + "-user",
+		"password": password,
+		"type":     "user",
+	}, "")
+
+	userId := userSignupData["userId"]
+
+	_, userSigninData := doRequest(t, "POST", BACKEND_URL+"/api/v1/signin", map[string]interface{}{
+		"username": username + "-user",
+		"password": password,
+	}, "")
+
+	userToken := userSigninData["token"].(string)
+
+	_, el1 := doRequest(t, "POST", BACKEND_URL+"/api/v1/admin/element", map[string]interface{}{
+		"imageUrl": "https://test.com/a.png",
+		"width":    1,
+		"height":   1,
+		"static":   true,
+	}, adminToken)
+
+	element1Id := el1["id"].(string)
+
+	_, el2 := doRequest(t, "POST", BACKEND_URL+"/api/v1/admin/element", map[string]interface{}{
+		"imageUrl": "https://test.com/b.png",
+		"width":    1,
+		"height":   1,
+		"static":   true,
+	}, adminToken)
+
+	element2Id := el2["id"].(string)
+
+	_, mapData := doRequest(t, "POST", BACKEND_URL+"/api/v1/admin/map", map[string]interface{}{
+		"thumbnail":  "https://thumbnail.com/a.png",
+		"dimensions": "100x200",
+		"name":       "Default space",
+		"defaultElements": []map[string]interface{}{
+			{"elementId": element1Id, "x": 20, "y": 20},
+			{"elementId": element1Id, "x": 18, "y": 20},
+			{"elementId": element2Id, "x": 19, "y": 20},
+		},
+	}, adminToken)
+
+	mapId := mapData["id"].(string)
+
+	_, spaceData := doRequest(t, "POST", BACKEND_URL+"/api/v1/space", map[string]interface{}{
+		"name":       "Test",
+		"dimensions": "100x200",
+		"mapId":      mapId,
+	}, userToken)
+
+	spaceId := spaceData["spaceId"].(string)
+
+	t.Run("Incorrect spaceId returns 400", func(t *testing.T) {
+		resp, _ := doRequest(t, "GET", BACKEND_URL+"/api/v1/space/123invalid", nil, userToken)
+
+		if resp.StatusCode != 400 {
+			t.Fatalf("expected 400 got %d", resp.StatusCode)
+		}
+	})
+
+	t.Run("Correct spaceId returns all elements", func(t *testing.T) {
+		_, data := doRequest(t, "GET", BACKEND_URL+"/api/v1/space/"+spaceId, nil, userToken)
+
+		if data["dimensions"] != "100x200" {
+			t.Fatal("dimensions mismatch")
+		}
+
+		elements := data["elements"].([]interface{})
+		if len(elements) != 3 {
+			t.Fatalf("expected 3 elements got %d", len(elements))
+		}
+	})
+
+	t.Run("Delete endpoint deletes element", func(t *testing.T) {
+		_, data := doRequest(t, "GET", BACKEND_URL+"/api/v1/space/"+spaceId, nil, userToken)
+		elements := data["elements"].([]interface{})
+		firstElement := elements[0].(map[string]interface{})
+		elementId := firstElement["id"].(string)
+
+		doRequest(t, "DELETE", BACKEND_URL+"/api/v1/space/element", map[string]interface{}{
+			"id": elementId,
+		}, userToken)
+
+		_, newData := doRequest(t, "GET", BACKEND_URL+"/api/v1/space/"+spaceId, nil, userToken)
+		newElements := newData["elements"].([]interface{})
+
+		if len(newElements) != 2 {
+			t.Fatalf("expected 2 elements got %d", len(newElements))
+		}
+	})
+
+	t.Run("Adding element outside dimensions fails", func(t *testing.T) {
+		resp, _ := doRequest(t, "POST", BACKEND_URL+"/api/v1/space/element", map[string]interface{}{
+			"elementId": element1Id,
+			"spaceId":   spaceId,
+			"x":         10000,
+			"y":         210000,
+		}, userToken)
+
+		if resp.StatusCode != 400 {
+			t.Fatalf("expected 400 got %d", resp.StatusCode)
+		}
+	})
+
+	t.Run("Adding element works", func(t *testing.T) {
+		doRequest(t, "POST", BACKEND_URL+"/api/v1/space/element", map[string]interface{}{
+			"elementId": element1Id,
+			"spaceId":   spaceId,
+			"x":         50,
+			"y":         20,
+		}, userToken)
+
+		_, newData := doRequest(t, "GET", BACKEND_URL+"/api/v1/space/"+spaceId, nil, userToken)
+		newElements := newData["elements"].([]interface{})
+
+		if len(newElements) != 3 {
+			t.Fatalf("expected 3 elements got %d", len(newElements))
+		}
+	})
+
+	_ = adminId
+	_ = userId
 }
