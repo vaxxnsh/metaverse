7203047e63a9c86fe3f33466f62033867e5934da
vaxxnsh <kakshit817@gmail.com>
2026-02-19 23:54:47 +0530

    chore: code clean up

---
+++ services/api/cmd/server/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+}
---
+++ services/api/internals/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string
+	Port        string
+	DBURL       string
+	JWTSecret   string
+	ReadTimeout time.Duration
+}
+
+func Load() *Config {
+	cfg := &Config{
+		Env:         getEnv("ENV", "development"),
+		Port:        getEnv("PORT", "8080"),
+		DBURL:       getEnv("DATABASE_URL", ""),
+		JWTSecret:   getEnv("JWT_SECRET", "supersecret"),
+		ReadTimeout: 5 * time.Second,
+	}
+
+	if cfg.DBURL == "" {
+		log.Fatal("DATABASE_URL is required")
+	}
+
+	return cfg
+}
+
+func getEnv(key string, fallback string) string {
+	val, exists := os.LookupEnv(key)
+	if !exists {
+		return fallback
+	}
+	return val
+}
